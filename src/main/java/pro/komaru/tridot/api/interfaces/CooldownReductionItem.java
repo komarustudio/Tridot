7a6d6b7063b20a7f9595bd57fe98d89f2d3540e4
Auriny <49393428+Auriny@users.noreply.github.com>
2026-07-22 23:47:32 +0600

    feat(master): refine overdrive enchantment balance

--- src/main/java/pro/komaru/tridot/api/interfaces/CooldownReductionItem.java
+++ src/main/java/pro/komaru/tridot/api/interfaces/CooldownReductionItem.java
@@ -8,10 +8,8 @@ public interface CooldownReductionItem{
         int level = stack.getEnchantmentLevel(EnchantmentsRegistry.OVERDRIVE.get());
         float modifier = 1.0f;
         if (level > 0) {
-            modifier = 0.90f;
-            if (level > 1) {
-                modifier -= (level - 1) * 0.05f;
-            }
+            modifier = 0.95f;
+            if (level > 1) modifier -= (level - 1) * 0.02f;
         }
 
         return Math.max(0, Math.round(cooldown * modifier));
